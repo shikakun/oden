2b522f913019c2a31b72fd07a3d1cbfcdc560fdf
shikakun <shikakun@shikakun.com>
2025-01-24 10:20:35 +0900

    Floating UIを使用してoverlayの位置を決めるようにした

--- src/components/ActionMenu/ActionMenu.tsx
+++ src/components/ActionMenu/ActionMenu.tsx
@@ -1,3 +1,4 @@
+import { autoUpdate, flip, shift, useFloating } from '@floating-ui/react-dom';
 import React, {
   createContext,
   PropsWithChildren,
@@ -12,18 +13,28 @@ interface ActionMenuContextProps {
   isOpen: boolean;
   toggle: () => void;
   close: () => void;
+  refs: ReturnType<typeof useFloating>['refs'];
+  floatingStyles: ReturnType<typeof useFloating>['floatingStyles'];
 }
 
 const ActionMenuContext = createContext<ActionMenuContextProps | null>(null);
 
 const ActionMenuProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const { refs, floatingStyles } = useFloating({
+    open: isOpen,
+    middleware: [flip(), shift()],
+    whileElementsMounted: autoUpdate,
+    placement: 'bottom-start',
+  });
 
   const toggle = () => setIsOpen((prev) => !prev);
   const close = () => setIsOpen(false);
 
   return (
-    <ActionMenuContext.Provider value={{ isOpen, toggle, close }}>
+    <ActionMenuContext.Provider
+      value={{ isOpen, toggle, close, refs, floatingStyles }}
+    >
       <div className={styles.root} role='menu' aria-hidden={!isOpen}>
         {children}
       </div>
@@ -42,13 +53,14 @@ const useActionMenuContext = (): ActionMenuContextProps => {
 };
 
 const ActionMenuButton: React.FC<ButtonProps> = ({ children, ...props }) => {
-  const { toggle, isOpen } = useActionMenuContext();
+  const { toggle, isOpen, refs } = useActionMenuContext();
 
   return (
     <Button
       onClick={toggle}
       aria-haspopup='true'
       aria-expanded={isOpen}
+      ref={refs.setReference}
       {...props}
     >
       {children}
@@ -57,10 +69,16 @@ const ActionMenuButton: React.FC<ButtonProps> = ({ children, ...props }) => {
 };
 
 const ActionMenuOverlay: React.FC<PropsWithChildren> = ({ children }) => {
-  const { isOpen } = useActionMenuContext();
+  const { isOpen, refs, floatingStyles } = useActionMenuContext();
 
   return isOpen ? (
-    <div className={styles.overlay} role='menu' aria-hidden={!isOpen}>
+    <div
+      className={styles.overlay}
+      role='menu'
+      aria-hidden={!isOpen}
+      ref={refs.setFloating}
+      style={floatingStyles}
+    >
       {children}
     </div>
   ) : null;

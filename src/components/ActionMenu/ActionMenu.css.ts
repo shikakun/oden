c80cf51a174d44cc01a9a4693cf7a597db690200
shikakun <shikakun@shikakun.com>
2025-01-25 09:11:24 +0900

    overlayの最大幅を指定した

--- src/components/ActionMenu/ActionMenu.css.ts
+++ src/components/ActionMenu/ActionMenu.css.ts
@@ -8,6 +8,9 @@ export const root = style({
 
 export const overlay = style({
   position: 'absolute',
+  width: 'max-content',
+  maxWidth: '100vw',
+  overflowWrap: 'break-word',
   backgroundColor: Color.background.page.light,
   boxShadow: Shadow.css[2].light,
   zIndex: 100,
